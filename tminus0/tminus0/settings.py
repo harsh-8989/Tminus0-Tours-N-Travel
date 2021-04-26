599d519dc4b48d8a6970c4985c320e418c8bcc80
bchetanrao <chetanraocr7@gmail.com>
2021-04-26 20:42:13 +0530

    some minor changes

--- tminus0/tminus0/settings.py
+++ tminus0/tminus0/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ToursNTravels',
         'USER': 'root',
-        'PASSWORD': "password",
+        'PASSWORD': "chetan7@IIITA",
         'HOST': 'localhost',
         'PORT': '3306'
     }
