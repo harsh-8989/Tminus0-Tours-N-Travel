ebed49d289ae9fda294a0aacfa61c67f689510ed
harsh-8989 <harshmahajan8989@gmail.com>
2021-04-26 18:14:51 +0530

    conflict

--- tminus0/developer/models.py
+++ tminus0/developer/models.py
@@ -7,3 +7,4 @@ class user_admin(models.Model):
     username = models.CharField(max_length=40, default="")
     email = models.CharField(max_length=35, unique=True, primary_key=True)
     password = models.CharField(max_length=20)
+    # gello
