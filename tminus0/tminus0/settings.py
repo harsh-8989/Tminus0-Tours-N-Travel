82f1cf376b0ace147413f6749848665441c269c7
bchetanrao <chetanraocr7@gmail.com>
2021-05-10 13:19:22 +0530

    updates

--- tminus0/tminus0/settings.py
+++ tminus0/tminus0/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ToursNTravel',
         'USER': 'postgres',
-        'PASSWORD': "password",
+        'PASSWORD': "chetan7@IIITA",
         'HOST': 'localhost',
         'PORT': '5432'
     }
